cb1b471bfc7d79e599084d2412e142f7f1f75ee6
hanseungheon <110355039+hhhaan@users.noreply.github.com>
2025-08-03 16:19:38 +0900

    .

--- src/widgets/playlist/index.tsx
+++ src/widgets/playlist/index.tsx
@@ -3,7 +3,7 @@ export const Playlist = () => {
         <iframe
             allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
             height="450"
-            style={{ width: '100%', maxWidth: '660px', overflow: 'hidden', borderRadius: '10px' }}
+            style={{ width: '100%', maxWidth: '660px', overflow: 'hidden', borderRadius: '10px', marginTop: '40px' }}
             sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
             src="https://embed.music.apple.com/kr/playlist/espresso-is-comming/pl.u-ZmblxWruVr42erq?l=en-GB"
         ></iframe>
